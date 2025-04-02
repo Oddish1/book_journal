ce129de910d29b8397c3fc492757a8d9d68ba408
Austin Winslow <austin@austinwinslow.com>
2025-04-02 15:01:26 -0700

    finished library data models, registered with admin, and migrated db

--- library/models.py
+++ library/models.py
@@ -1,75 +1,116 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return "user_{0}/{1}".format(instance.user.id, filename)
+    return f'user_{instance.id}/{filename}'
 
 # data model for the user
-class User(models.Model):
-    # a user-defined email
-    email = models.EmailField()
-    # a user-defined username
-    username = models.CharField(max_length=200)
-    # the hashed version of a user-defined password
-    password_hashed = models.CharField(max_length=200)
-    # date-time object representing the time the account was created
-    created_at = models.DateTimeField(auto_now=True)
-    # a date-time object representing the last time the account password was
-    # reset
-    last_password_reset = models.DateTimeField(auto_now=True)
-    # the user's first name; optional
-    first_name = models.CharField(max_length=200, blank=True)
-    # the user's last name; optional
-    last_name = models.CharField(max_length=200, blank=True)
+class User(AbstractUser):
     # a user-uploaded profile picture; optional
     profile_picture = models.ImageField(upload_to=user_directory_path,
-                                        blank=True)
+                                        null=True)
     # a user-defined bio; optional
-    bio = models.TextField(max_length=5000, blank=True)
-    # define preference choices
-    #
-    # the user's preferences
-    preferences = 
-    # a boolean to depict wether the user is currently active or not
-    is_active = models.BooleanField()
-    # a date-time object representing the last time the user logged in
-    last_login = models.DateTimeField(auto_now=True)
-    # define account type choices
-    ADMIN = "ad"
-    STANDARD_USER = "us"
-    ACCOUNT_TYPE_CHOICES = {
-            ADMIN: "Administrator",
-            STANDARD_USER: "Standard User",
-            }
+    bio = models.TextField(max_length=5000, null=True)
+    # a date-time object representing the last time the account password was
+    # reset
+    last_password_reset = models.DateTimeField(auto_now_add=True)
     # the type of account; defaults to standard user
-    account_type = models.CharField(max_length=2, choices=ACCOUNT_TYPE_CHOICES,
-                                    default=STANDARD_USER)
+    account_type = models.CharField(max_length=2,
+                                    choices=[("ad", "Administrator"), ("us", "Standard User")],
+                                    default="us")
+
+    def __str__(self):
+        return self.username
 
 # data model for book lists (currently reading, read, etc.)
 class List(models.Model):
     # a forein key linking to the User.id (auto-generated primary key by Django)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # a disply name for the list
-    list_name =models.CharField(max_length=200)
+    name =models.CharField(max_length=200, default='My List')
     # a boolean representing if the list visible to the public or not (default
     # false)
     is_public = models.BooleanField(default=False)
+    # a text field representing a list description
+    description = models.TextField(max_length=2000, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Covers(models.Model):
+    # represents the cover image
+    image = models.ImageField()
+
+
+class Genres(models.Model):
+    # represents the genre (sci-fi, history, historical fiction)
+    genre = models.CharField(max_length=200)
+
+
+class Authors(models.Model):
+    # represents the author's name
+    name = models.CharField(max_length=200)
+    # datetime.date object representing the date the author was born
+    birth_date = models.DateField()
+    # datetime.date object representing the date the author died
+    death_date = models.DateField(null=True)
+    # text field representing the author's biography
+    biography = models.TextField(max_length=2000, null=True)
+
+
+class Book(models.Model):
+    # foreign key linking to Genres.id representing the main genre of the book
+    main_genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genres, related_name="books")
+    # foreign key linking to Covers.id representing the thumbnail cover
+    thumbnail_cover = models.ForeignKey(Covers, on_delete=models.CASCADE, null=True)
+    covers = models.ManyToManyField(Covers, related_name="covers")
+    # foreign key linking to Authors.id representing the main author
+    main_author = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Authors, related_name="authors")
+    # foreign key linking to List.id representing the list the book is in
+    list = models.ForeignKey(List, on_delete=models.CASCADE, null=True)
+    # represents the book title, max length of 500 characters
+    title = models.CharField(max_length=500)
+    # represents the book subtitle, max length of 500 characters
+    subtitle = models.CharField(max_length=500, null=True)
+    # integer representing the number of pages in the book
+    page_count = models.IntegerField(null=True)
+    # float representing the average rating given by users
+    average_rating = models.FloatField(null=True)
+    # integer representing the number of ratings given by users
+    ratings_count = models.IntegerField(default=0)
+    # represents the book publisher
+    publisher = models.CharField(max_length=500, null=True)
+    # represents the date published
+    published_date = models.DateField(null=True)
+    # represents a summary/description of the book
+    description = models.TextField(max_length=1000, null=True)
+    # represents the print type of the book (Hardcover, EBook, Mass Market Paperback)
+    print_type = models.CharField(max_length=200, null=True)
+    # represents the language the book is published in
+    language = models.CharField(max_length=200, null=True)
+    # represents the isbn of the book
+    isbn = models.CharField(max_length=50, null=True)
 
 
 # data model for a user created journal entry
 class Journal(models.Model):
     # a foreign key linking the User.id
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # a foreign key linking the Book.id
-    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     # a title for the journal entry
-    title = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=200, null=True)
     # a page number representing the page they are on
     page = models.IntegerField()
     # text space for the user to make a journal entry
-    journal_text = models.TextField(max_length=10000, blank=True)
+    journal_text = models.TextField(max_length=10000, null=True)
     # a datetime object representing when the journal entry was created
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     # a datetime object representing when the journal entry was last edited
     edited_at = models.DateTimeField(auto_now=True)
     # a boolean representing if the journal entry is visible to the public or
@@ -78,55 +119,60 @@ class Journal(models.Model):
     # a boolean representing if the journal entry able to be viewed by the
     # public or not, for moderation purposes
     is_approved = models.BooleanField(default=False)
-
-
-class JournalTags(models.Model):
-
+    tags = models.ManyToManyField("Tags", related_name="journals")
 
 
 class UserFollow(models.Model):
-
+    # foreign key linking to User.id representing the user following
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followers")
+    # foreign key linking to User.id representing the user that has been
+    # followed
+    followed = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following")
+    # auto-generated datetime object for when the follow event occurs
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class UserRecommendations(models.Model):
-
-
-
-class Book(models.Model):
-
-
-
-class ReviewTags(models.Model):
+    # foreign key linking to User.id representing the user the recommendation is
+    # for
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # foreign key linking to Book.id representing the book being recommended
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    # auto-generated datetime object for when the recommendation is being made
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 
 class Tags(models.Model):
-
-
-
-class Covers(models.Model):
-
-
-
-class BookCovers(models.Model):
-
-
-
-class Genres(models.Model):
-
-
-
-class BookGeneres(models.Model):
-
-
-
-class Authors(models.Model):
-
-
-
-class BookAuthors(models.Model):
+    # represents the tag text
+    tag = models.CharField(max_length=200)
 
 
 
 class Reviews(models.Model):
+    # foreign key linking to Book.id
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    # foreign key linking to User.id
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # represents a float rating given by the user
+    rating = models.FloatField(null=True,
+                               validators=[MinValueValidator(0), MaxValueValidator(5)])
+    # represents a title for the review
+    title = models.CharField(max_length=200, null=True)
+    # represents the text of the review
+    review = models.TextField(max_length=5000, null=True)
+    # a datetime object representing the timestamp the review was created at
+    created_at = models.DateTimeField(auto_now_add=True)
+    # a datetime object representing the timestamp the review was last edited
+    updated_at = models.DateTimeField(auto_now=True)
+    # a boolean representing if the review is approved (used for moderation)
+    is_approved = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tags", related_name="reviews")
+
+
+class BooksOwned(models.Model):
+    # foreign key linking to User.id
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # foreign key linking to Book.id
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
